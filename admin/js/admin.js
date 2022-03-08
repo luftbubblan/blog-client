835e5ffffd328d875d8efac2a4940f99eb308449
Joni <rautiojonathan@gmail.com>
2022-03-08 22:31:50 +0100

    async/await and generated the content with js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,37 +1,41 @@
-console.log("checking if connected");
+window.onload = () => fetchAllPosts();
 
-/* window.onload = function () {
-    fetchAllPuns();
+const errDiv = document.querySelector("#errDiv");
+
+async function fetchAllPosts() {
+  try {
+    const response = await fetch("http://localhost:5000/posts/");
+    const posts = await response.json();
+    console.log(posts);
+
+    if (!response.ok) {
+      throw new Error("Error!");
+    }
+
+    let html = "";
+
+    for (let post of posts) {
+      html += ` 
+      <tr>
+      <td>${post.title}</td>
+      <td>${post.author}</td>
+      <td>${post.date.slice(0, 10)} ${post.date.slice(11, 16)}</td>
+      <td><a href="update-post.html?id=${
+        post._id
+      }">Update</a> / <a href="">Delete</a></td>
+      </tr>
+      
+      `;
+    }
+
+    document.querySelector("#tBody").innerHTML = html;
+  } catch (err) {
+    console.log(err);
+    errDiv.innerHTML = `There was an error (${err}). Try again!`;
+  }
 }
 
-async function fetchAllPuns() {
-    const response = await fetch('https://puns-app.herokuapp.com/puns');
-    const puns = await response.json();
-    //creates empty output
-    let outputListOfPuns = "";
-
-    //fills the output with all puns that are fetched
-    $.each(puns, function(key, value) {
-        outputListOfPuns += (`
-            <li class="list-group-item">
-                <p>
-                    ${value.content} <br>
-                    <span class="date">- ${value.date}</span>
-                </p>
-                
-                <div>
-                    <a href="update-pun.html?id=${value._id}" id="update">Update</a> |
-                    <a href="" data-id="${value._id}" id="delete">Delete</a> 
-                </div>
-            </li>
-        `);   
-    });
-    //prints the puns to the site
-    $('#pun-list').html(outputListOfPuns);
-    //runs counter function
-    counter(puns.length);
-    
-    $('a').click(async function(e) {
+/*    $('a').click(async function(e) {
         //preventsDefault from Del-btn, fetches correct pun and deletes it from API
         if($(this).html() === 'Delete') {
             e.preventDefault();
@@ -55,11 +59,11 @@ async function fetchAllPuns() {
         }
         //runs the whole code again (updates the site without reloading the site)
         //runs with both Delete and Refresh btn
-        fetchAllPuns();
+        fetchAllPosts();
     });
 }
 
 //shows how many puns is on the site
 function counter(nbrOfPuns) {
     $('#count').html(`${nbrOfPuns} puns found`);
-} */
+}  */
