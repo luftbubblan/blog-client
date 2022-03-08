ffac04969a8171d9dd9a0d0a85e10bc25f29ccb5
Joni <rautiojonathan@gmail.com>
2022-03-08 13:27:50 +0100

    updated some text

--- js/posts.js
+++ js/posts.js
@@ -1,16 +1,16 @@
 window.onload = function () {
-    fetchAllPuns();
-}
+  fetchAllPuns();
+};
 
 async function fetchAllPuns() {
-    const response = await fetch('https://puns-app.herokuapp.com/puns');
-    const puns = await response.json();
-    //creates empty output
-    let outputListOfPuns = "";
+  const response = await fetch("https://puns-app.herokuapp.com/puns");
+  const puns = await response.json();
+  //creates empty output
+  let outputListOfPuns = "";
 
-    //fills the output with all puns that are fetched
-    $.each(puns, function(key, value) {
-        outputListOfPuns += (`
+  //fills the output with all puns that are fetched
+  $.each(puns, function (key, value) {
+    outputListOfPuns += `
             <li class="list-group-item">
                 <p>
                     ${value.content} <br>
@@ -22,42 +22,44 @@ async function fetchAllPuns() {
                     <a href="" data-id="${value._id}" id="delete">Delete</a> 
                 </div>
             </li>
-        `);   
-    });
-    //prints the puns to the site
-    $('#pun-list').html(outputListOfPuns);
-    //runs counter function
-    counter(puns.length);
-    
-    $('a').click(async function(e) {
-        //preventsDefault from Del-btn, fetches correct pun and deletes it from API
-        if($(this).html() === 'Delete') {
-            e.preventDefault();
+        `;
+  });
+  //prints the puns to the site
+  $("#pun-list").html(outputListOfPuns);
+  //runs counter function
+  counter(puns.length);
 
-            try {
-                const response = await fetch(`https://puns-app.herokuapp.com/puns/${$(this)[0].dataset.id}`, {
-                    method: 'DELETE'
-                })
-        
-                if(!response.ok) {
-                    throw new Error('API Error');
-                }
-        
-            } catch(error) {
-                console.log(error);
-            }
+  $("a").click(async function (e) {
+    //preventsDefault from Del-btn, fetches correct pun and deletes it from API
+    if ($(this).html() === "Delete") {
+      e.preventDefault();
 
-        //preventsDefault from refresh btn
-        } else if($(this).html() === 'Refresh') {
-            e.preventDefault();
+      try {
+        const response = await fetch(
+          `https://puns-app.herokuapp.com/puns/${$(this)[0].dataset.id}`,
+          {
+            method: "DELETE",
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("API Error");
         }
-        //runs the whole code again (updates the site without reloading the site)
-        //runs with both Delete and Refresh btn
-        fetchAllPuns();
-    });
+      } catch (error) {
+        console.log(error);
+      }
+
+      //preventsDefault from refresh btn
+    } else if ($(this).html() === "Refresh") {
+      e.preventDefault();
+    }
+    //runs the whole code again (updates the site without reloading the site)
+    //runs with both Delete and Refresh btn
+    fetchAllPuns();
+  });
 }
 
 //shows how many puns is on the site
 function counter(nbrOfPuns) {
-    $('#count').html(`${nbrOfPuns} puns found`);
+  $("#count").html(`${nbrOfPuns} st artiklar funna`);
 }
