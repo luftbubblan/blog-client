8ed15294c54b6020d205f365dc0a3a51367997e5
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-03-09 15:48:20 +0100

    Co-authored-by: Malin von Engelhardt Volle <malin.vonengelhardt_volle@cmeducations.se> Co-authored-by: Anna Nilsson <annamcn98@users.noreply.github.com>

--- admin/js/update-post.js
+++ admin/js/update-post.js
@@ -4,6 +4,15 @@ window.onload = function () {
 
 async function updatePost(e) {
     e.preventDefault();
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    //fetches the specific pun that should be updated
+    // const response = await fetch(`https://puns-app.herokuapp.com/puns/${urlParams.get("id")}`);
+    // const pun = await response.json();
+
+    //fills out the textbox with the text from the fetched pun
+    // $('#content-textarea').val(pun.content);
     
     let allTags = [];
     //snaps up the form
@@ -11,10 +20,7 @@ async function updatePost(e) {
     $.each(formData.getAll('tags'), function(key, tag) {
         allTags.push(tag);
     })
-    
-
-
-    
+ 
     const contentObj = {
         title: formData.get('title'),
         author: formData.get('author'),
@@ -46,3 +52,57 @@ async function updatePost(e) {
         console.log(error);
     }
 }
+
+
+
+
+/**
+ * 
+ * 
+    async function updatePun() {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    //fetches the specific pun that should be updated
+    const response = await fetch(`https://puns-app.herokuapp.com/puns/${urlParams.get("id")}`);
+    const pun = await response.json();
+
+    //fills out the textbox with the text from the fetched pun
+    $('#content-textarea').val(pun.content);
+
+    $('#update-pun-form').submit(async function(e) {
+        e.preventDefault();
+
+        //snaps up the form
+        const formData = new FormData(e.target);
+        const contentObj = {
+            content: formData.get('content')
+        };
+        //converts the content to JSON
+        const JSONContent = JSON.stringify(contentObj);
+
+        try {
+            //PATCHes the JSON to the API in a new pun
+            const response = await fetch(`https://puns-app.herokuapp.com/puns/${urlParams.get("id")}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSONContent
+            });
+
+            if(!response.ok) {
+                throw new Error('API Error');
+            }
+
+            //sends the user back to index site
+            window.location.replace('index.html');
+
+        } catch(error) {
+            console.log(error);
+        }
+    });
+}
+ * 
+ * 
+ */
+
