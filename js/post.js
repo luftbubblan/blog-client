2f186f9666bcbd77a3b714c3a5cc8ec21bbccb79
Joni <rautiojonathan@gmail.com>
2022-03-10 13:09:55 +0100

    sidebar post.js

--- js/post.js
+++ js/post.js
@@ -8,23 +8,42 @@ async function readMore() {
     `http://localhost:5000/posts/${urlParams.get("id")}`
   );
   const post = await response.json();
-  console.log(post);
 
-  $("#blogPost").html(
+  $("#main-content").html(
     `
-			<h2 class="post-title">${post.title}</h2>
-			
-			<span id="author">By: ${post.author}</span>
-		
-			<p class="post-content">${post.content}</p>
-
-			<div id="tags">
-        Published: ${post.date.slice(0, 10)} ${post.date.slice(11, 16)} <br>
-        Tags: ${showTagsCapitalizeAddSpace(post.tags)}
+    <div id="grid-container">
+      <div id="blogPost">
+        <h2 class="post-title">${post.title}</h2>
+        <span id="author">By: ${post.author}</span>
+        <p class="post-content">${post.content}</p>
+        <div id="tags">
+          Published: ${post.date.slice(0, 10)} ${post.date.slice(11, 16)} <br>
+          Tags: ${showTagsCapitalizeAddSpace(post.tags)}
       </div>
+    </div>
+      <aside id="sidebar">
+        <h2>About us</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, nostrum?</p>
+        <h3>Latest posts</h3>
+         
+      </aside>
+    </div>
 		`
   );
 }
+
+/* async function gettingPosts() {
+  const response = await fetch("http://localhost:5000/posts/");
+  const posts = await response.json();
+  console.log(posts[1].title);
+  let html = "";
+
+  for (let i = 0; i < 5; i++) {
+    console.log("Hej");
+  }
+}
+
+gettingPosts(); */
 
 //function for showing tags (leaving empty ones out), capitalize first tag character and add space after ","
 function showTagsCapitalizeAddSpace(array) {
