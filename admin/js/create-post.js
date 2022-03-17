b6cf06e5541ae9dc4d2dbab30d6adadb111cbbff
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-03-17 20:12:50 +0100

    ongoing

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,104 +1,119 @@
-window.onload = function () {
-  authors();
-  $("#createPostForm").submit(createPost);
+window.onload = async function () {
+	const data = await fetchRandomPictureAndAuthors();
+	const photoData = data[0];
+	const approvedAuthors = data[1];
+
+	createApprovedAuthorsList(approvedAuthors);
+
+	submitNewPost(photoData);
 };
 
-async function authors() {
-  const authors = await fetch("authors.txt");
-  const authorsText = await authors.text();
-  const approvedAuthors = authorsText.split(",");
 
-  for (let approvedAuthor of approvedAuthors) {
-    $("#authorList").append(`
-        <option value="${approvedAuthor}">${approvedAuthor}</option>
-        `);
-  }
+async function fetchRandomPictureAndAuthors() {
+	try {
+		const data = await Promise.all([
+			//fetch random gardening picture
+			fetch('https://api.unsplash.com/photos/random/?client_id=ZnDEJlu-KLWTsvfpRtkxmrG6zkv4LIiqLB9acm7hBV8&query=gardening').then(resp => resp.json()),
+								//Temporary fetch unspash is timed out, to many calls per hour
+								// fetch('https://random.dog/doggos').then(resp => resp.json()),
+			//fetches the list with approved authors
+			fetch('authors.txt').then(resp => resp.text())
+		]);
+
+		//creates an array with the approved authors
+		const approvedAuthors = data[1].split(",");
+		
+								// return [data[0][0], approvedAuthors];
+								//remove the above and use the under when using unspashAPI
+		return [data[0], approvedAuthors];
+
+
+	} catch (error) {
+		console.log(error);
+	}
 }
 
-async function createPost(e) {
-  e.preventDefault();
 
-  let allTags = [];
-  //snaps up the form
-  const formData = new FormData(e.target);
-  $.each(formData.getAll("tags"), function (key, tag) {
-    allTags.push(tag);
-  });
+function createApprovedAuthorsList(approvedAuthors) {
+	//appends every approved author to the Author menu
+	for (let approvedAuthor of approvedAuthors) {
+		$("#authorList").append(`<option value="${approvedAuthor}">${approvedAuthor}</option>`);
+	}
+}
 
-  //fetches a random gardening picture
-  const response = await fetch(
-    "https://api.unsplash.com/photos/random/?client_id=ZnDEJlu-KLWTsvfpRtkxmrG6zkv4LIiqLB9acm7hBV8&query=gardening"
-  );
-  const gardeningImage = JSON.stringify(await response.json());
 
-  const contentObj = {
-    title: formData.get("title"),
-    author: formData.get("author"),
-    content: formData.get("content"),
-    tags: allTags,
-    image: gardeningImage,
-  };
+async function submitNewPost(gardeningImage) {
+	$('#createPostForm').submit( async function(e) {
+		e.preventDefault();
 
-  //if title or content is just containing spaces cancel function
-  //if they contain stuff remove error
-  emptyField = false;
-  if (contentObj.title.trim() === "") {
-    $("#titleError").html("<div>You can not submit an empty Title</div>");
-    $("#titleError").removeAttr("hidden");
-    emptyField = true;
-  } else if (contentObj.title.trim().length > 60) {
-    console.log("more then 10");
-    $("#titleError").html(
-      "<div>The Title can only be 60 characters long</div>"
-    );
-    $("#titleError").removeAttr("hidden");
-    emptyField = true;
-  } else {
-    $("#titleError").attr("hidden", "true");
-  }
+		let allTags = [];
+		//snaps up the form
+		const formData = new FormData(e.target);
+		//snaps up all tags
+		$.each(formData.getAll("tags"), function (key, tag) {
+			allTags.push(tag);
+		});
 
-  if (contentObj.content.trim() === "") {
-    $("#contentError").html("<div>You can not submit empty Content</div>");
-    $("#contentError").removeAttr("hidden");
-    emptyField = true;
-  } else {
-    $("#contentError").attr("hidden", "true");
-  }
+		const contentObj = {
+			title: formData.get("title"),
+			author: formData.get("author"),
+			content: formData.get("content"),
+			tags: allTags,
+			image: gardeningImage,
+		};
 
-  if (emptyField) {
-    return false;
-  }
+		//variable used to abort function if a required field is empty
+		emptyField = false;
 
-  //converts the content to JSON
-  const JSONContent = JSON.stringify(contentObj);
-  console.log(JSONContent);
+		//if Title only contains spaces, show error message, emptyField = true
+		if (contentObj.title.trim() === "") {
+			$("#titleError").html("You can not submit an empty Title");
+			$("#titleError").removeAttr("hidden");
+			emptyField = true;
 
-  try {
-    const authors = await fetch("authors.txt");
-    const authorsText = await authors.text();
-    const approvedAuthors = authorsText.split(",");
+		//if Title contains more than 60 chars, show error message, emptyField = true
+		} else if (contentObj.title.trim().length > 60) {
+			$("#titleError").html("The Title can only be 60 characters long");
+			$("#titleError").removeAttr("hidden");
+			emptyField = true;
 
-    for (let approvedAuthor of approvedAuthors) {
-      $("#authorList").append(`
-            <option value="${approvedAuthor}">${approvedAuthor}</option>
-            `);
-    }
-    //POSTs the JSON to the API in a new post
-    const response = await fetch("http://localhost:5000/posts/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONContent,
-    });
+		//if Title error message was shown and the error has been corrected hide the error div
+		} else {
+			$("#titleError").attr("hidden", "true");
+		}
 
-    if (!response.ok) {
-      throw new Error("API Error");
-    }
+		//if Content only contains spaces, show error message, emptyField = true
+		if (contentObj.content.trim() === "") {
+			$("#contentError").html("You can not submit empty Content");
+			$("#contentError").removeAttr("hidden");
+			emptyField = true;
 
-    //sends the user back to admin site
-    window.location.replace("admin.html");
-  } catch (error) {
-    console.log(error);
-  }
+		//if Content error message was shown and the error has been corrected hide the error div
+		} else {
+			$("#contentError").attr("hidden", "true");
+		}
+
+		//if emptyField = true cancel the function(form is not submited, user gets error messages instead)
+		if (emptyField) {
+			return false;
+		}
+
+		//converts the content to JSON
+		const JSONContent = JSON.stringify(contentObj);
+
+		try {
+			// POSTs the JSON to the API as a new post
+			const response = await fetch("http://localhost:5000/posts/", {
+				method: "POST",
+				headers: {"Content-Type": "application/json"},
+				body: JSONContent
+			});
+
+		} catch (error) {
+			console.log(error);
+		}
+
+			//sends the user back to admin site
+			window.location.replace("admin.html");
+	});
 }
